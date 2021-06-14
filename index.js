5d251985d8065dafcdb7ed10046b5fae2acf9635
taporian <christapor.har@gmail.com>
2021-06-14 14:47:19 +0300

    Step 3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { Router } = require('express');
 const express = require('express');
 
 const app = express();
@@ -5,4 +6,24 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('ok.');
 });
+const router = express.Router()
+
+app.get('/test', (req, res)=>{
+    const response = {
+        status:200, message:"test"
+    };
+    res.send(response);
+
+});
+app.get('/time', (req, res)=>{
+    var date = new Date();
+    const response = {
+        status:200, message:date.getHours()+":"+date.getMinutes()
+    };
+    res.send(response);
+
+});
+
+
+
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
